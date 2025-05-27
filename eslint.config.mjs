66d9148517fead54ace5dff2b3fc670c26b15d10
Yossi Tsafar <yositsafar@gmail.com>
2025-05-28 02:39:07 +0300

    fix eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@next/next/no-html-link-for-pages": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+    }
+  }
 ];
 
 export default eslintConfig;
